cdaf5cd8060adac58a8cd999371e3cb4a2613dcf
zhaoepng <929616871@qq.com>
2019-10-08 09:39:59 +0800

    添加图的邻接表存储结构

--- DataStructure_C/data_structure/Graph.cpp
+++ DataStructure_C/data_structure/Graph.cpp
@@ -1,5 +1,6 @@
 #include "Graph.h"
-/* 创建无向网图 */
+#include <stdlib.h>
+/* 创建无向网图的邻接矩阵结构 */
 void CreateMGraph(MGraph *G)
 {
 	int i, j, k, w;
@@ -22,6 +23,36 @@ void CreateMGraph(MGraph *G)
 		scanf("%d,%d,%d", &i, &j, &w);	/* 输入边（vi,vj）上的权w */
 		G->arc[i][j] = w;
 		G->arc[j][i] = G->arc[i][j];	/* 对称矩阵 */
+	}
+
+}
+
+/* 创建无向图的邻接表结构 */
+void CreatALGraph(GraphAdjList *G)
+{
+	int i, j, k;
+	EdgeNode *e;	//边表结点
+	printf("请输入定点数和边数!\n");
+	scanf("%d,%d", &G->numVertexes, &G->numEdges);//输入定点数和边数
+	for (i = 0; i < G->numVertexes; i++)
+	{
+		scanf(&G->adjList[i].data);			//输入顶点信息
+		G->adjList[i].fristedge = NULL;		//将边表置为空表
+	}
+	for (k = 0; k < G->numEdges; k++)	//建立边表
+	{
+		printf("输入边（vi,vj）上的顶点序号：\n");
+		scanf("%d,%d", &i, &j);
+
+		e = (EdgeNode *)malloc(sizeof(EdgeNode));//向内存申请空间，生成边表结点
+		e->adjvex = j;	//邻接序号
+		e->next = G->adjList[i].fristedge;		//将e指针指向当前顶点指向的结点
+		G->adjList[i].fristedge = e;			//将当前顶点的指针指向 e 
+
+		e = (EdgeNode *)malloc(sizeof(EdgeNode));//向内存申请空间，生成边表结点
+		e->adjvex = i;	//邻接序号
+		e->next = G->adjList[j].fristedge;		//将e指针指向当前顶点指向的结点
+		G->adjList[j].fristedge = e;			//将当前顶点的指针指向 e 
 	}
 
 }
--- DataStructure_C/data_structure/Graph.h
+++ DataStructure_C/data_structure/Graph.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <stdio.h>
-//定义邻接矩阵（Adjacency Matrix）存储结构
 typedef char VertexType;	/* 顶点类型由用户定义 */
 typedef int EdgeType;		/* 边上的权值类型应有用户定义 */
+
+//定义邻接矩阵（Adjacency Matrix）存储结构
 #define MAXVEX 100			/* 最大顶点数 */
 #define INFINITY 65535		/* 用65535代替无穷 */
 
@@ -14,3 +15,27 @@ typedef struct
 }MGraph;
 
 void CreateMGraph(MGraph *G);/* 创建无向网图 */
+
+//定义邻接表（Adjacency List）存储结构
+typedef struct EdgeNode		/* 边表结点 */
+{
+	int adjvex;				/* 邻接顶点，存储该定点对应的下标 */
+	EdgeType weight;		/* 用于存储权值 */
+	struct EdgeNode *next;  /* 链域，指向下一个邻接点 */
+}EdgeNode;
+typedef struct VertexNode	/* 顶点表结点 */
+{
+	VertexType data;		/* 定点域，存储顶点信息 */
+	EdgeNode *fristedge;	/* 边表头指针 */
+}VertexNode, AdjList[MAXVEX];
+
+typedef struct
+{
+	AdjList adjList;
+	int numVertexes, numEdges;	/* 图中当前顶点数 */
+}GraphAdjList;
+
+
+
+
+
